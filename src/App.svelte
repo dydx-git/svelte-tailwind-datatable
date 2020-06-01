476e8b7591d90a06148eb04fba3c43be9bc067eb
Muhammad Taha <al.dighton@outlook.com>
2020-06-01 12:34:29 +0500

    Added form component

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,14 @@
 <script>
+  import Tailwindcss from "./Tailwindcss.svelte";
   import Datatable from "./datatable/DataTable.svelte";
   import data from "./datatable/data/data.js";
+  import Form from "./Form.svelte";
+  import { slide } from "svelte/transition";
+
+  let showEditForm = false;
 
   const handleRowClick = e => {
-    if (e.detail.target.name) console.log(e.detail.row);
+    if (e.detail.target.name) showEditForm = true;
   };
 
   let columndata = [
@@ -43,7 +48,7 @@
       field: function(data) {
         return (
           '<div class="py-2">' +
-          '<Button name="edit_button">Show dialog</Button>' +
+          '<Button class="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-l" name="edit_button">Show dialog</Button>' +
           "</div>"
         );
       },
@@ -53,10 +58,20 @@
   ];
 </script>
 
-<Datatable
-  rows="{data}"
-  title="Orders"
-  columns="{columndata}"
-  process="local"
-  on:row-click="{handleRowClick}"
-/>
+<Tailwindcss />
+
+{#if !showEditForm}
+  <div transition:slide>
+    <Datatable
+      rows="{data}"
+      title="Orders"
+      columns="{columndata}"
+      process="local"
+      on:row-click="{handleRowClick}"
+    />
+  </div>
+{:else}
+  <div>
+    <Form />
+  </div>
+{/if}
